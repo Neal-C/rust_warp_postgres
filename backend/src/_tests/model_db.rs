83cc1881d6068c580922f2aad3511a991a6f401e
Neal-C <pop6d531@duck.com>
2023-09-22 18:57:06 +0200

    added more to model dir

--- backend/src/_tests/model_db.rs
+++ backend/src/_tests/model_db.rs
@@ -1,7 +1,15 @@
 use super::initialize_database;
 #[tokio::test]
 async fn model_database_initialize_database() -> Result<(), Box<dyn std::error::Error>> {
-    initialize_database().await;
+    // ARRANGE
+    let database = initialize_database().await?;
+
+    //ACT
+    let result = sqlx::query("SELECT * FROM todo")
+        .fetch_all(&database)
+        .await?;
+
+    assert_eq!(2, result.len(), "number of seed todos");
 
     Ok(())
 }
--- backend/src/model/db.rs
+++ backend/src/model/db.rs
@@ -1,21 +1,71 @@
-use std::{fs, time::Duration};
+use std::{fs, path::PathBuf, time::Duration};
 
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 pub type PostgresDatabase = Pool<Postgres>;
 
+// Refactor to use .env variables
 const POSTGRES_HOST: &str = "localhost";
 const POSTGRES_ROOT_DATABASE: &str = "postgres";
 const POSTGRES_ROOT_USER: &str = "postgres";
 const POSTGRES_ROOT_PASSWORD: &str = "postgres";
 
+// Refactor to use .env variables
+const POSTGRES_APP_DATABASE: &str = "app";
+const POSTGRES_APP_USER: &str = "app";
+const POSTGRES_APP_PASSWORD: &str = "app";
+const POSTGRES_APP_MAX_CONNECTIONS: u32 = 5;
+
+// Refactor to use .env variables
+const SQL_DIRECTORY: &str = "sql/";
+const SQL_RECREATE: &str = "sql/00-recreate-db.sql";
+
 pub async fn initialize_database() -> Result<PostgresDatabase, sqlx::Error> {
+    // -- Create the database with POSTGRES_ROOT --> in development only
+    {
+        let root_db = new_database_pool(
+            POSTGRES_HOST,
+            POSTGRES_ROOT_DATABASE,
+            POSTGRES_ROOT_USER,
+            POSTGRES_ROOT_PASSWORD,
+            1,
+        )
+        .await?;
+        execute_sql_file(&root_db, SQL_RECREATE).await?;
+    }
+
+    let app_database = new_database_pool(
+        POSTGRES_HOST,
+        POSTGRES_APP_DATABASE,
+        POSTGRES_APP_USER,
+        POSTGRES_APP_PASSWORD,
+        POSTGRES_APP_MAX_CONNECTIONS,
+    )
+    .await?;
+
+    let mut paths: Vec<PathBuf> = fs::read_dir(SQL_DIRECTORY)?
+        .filter_map(|element| element.ok().map(|e| e.path()))
+        .collect::<Vec<PathBuf>>();
+
+    paths.sort();
+
+    for path in paths {
+        if let Some(path) = path.to_str() {
+            if std::path::Path::new(path)
+                .extension()
+                .map_or(false, |ext| ext.eq_ignore_ascii_case("sql")) && path != SQL_RECREATE {
+                execute_sql_file(&app_database, path).await?;
+            }
+        }
+    }
+
+    // returning the app db
     new_database_pool(
         POSTGRES_HOST,
-        POSTGRES_ROOT_DATABASE,
-        POSTGRES_ROOT_USER,
-        POSTGRES_ROOT_PASSWORD,
-        1,
+        POSTGRES_APP_DATABASE,
+        POSTGRES_APP_USER,
+        POSTGRES_APP_PASSWORD,
+        POSTGRES_APP_MAX_CONNECTIONS,
     )
     .await
 }
@@ -36,10 +86,10 @@ async fn new_database_pool(
         .await
 }
 
-async fn seed_database(database: &PostgresDatabase, file: &str) -> Result<(), sqlx::Error> {
+async fn execute_sql_file(database: &PostgresDatabase, file: &str) -> Result<(), sqlx::Error> {
     // Read the file
     let content = fs::read_to_string(file).map_err(|error| {
-        println!("Error reading: {}, (cause :{:?}", file, error);
+        println!("Error reading: {file}, (cause :{error:?}");
         error
     })?;
 
