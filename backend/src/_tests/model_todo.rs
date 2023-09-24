5d2d2ac39a70bf9dd855f569d3d1f3e0cae5a221
Neal-C <pop6d531@duck.com>
2023-09-24 17:16:36 +0200

    add(create functionality+tests): added create function for the model access controller + its tests

--- backend/src/_tests/model_todo.rs
+++ backend/src/_tests/model_todo.rs
@@ -1,5 +1,24 @@
-use super::ModelAccessController;
+use super::{ModelAccessController, PartialTodo};
 use crate::model::db::initialize_database;
+
+#[tokio::test]
+async fn model_todo_create() -> Result<(), Box<dyn std::error::Error>> {
+    // ARRANGE
+    let database = initialize_database().await?;
+
+    let data_fixture = PartialTodo {
+        title: Some("test - model_todo_create 1".into()),
+        ..PartialTodo::default()
+    };
+
+    // ACT
+    let todo_created = ModelAccessController::create(&database, data_fixture.clone()).await?;
+
+    assert!(todo_created.id >= 1000, "ID should be >= 1000");
+    assert_eq!(data_fixture.title.unwrap(), todo_created.title);
+
+    Ok(())
+}
 
 #[tokio::test]
 async fn model_todo_list() -> Result<(), Box<dyn std::error::Error>> {
@@ -14,13 +33,13 @@ async fn model_todo_list() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("\n\n-->> {result:#?}");
 
-	assert_eq!(101, result[0].id);
-	assert_eq!(123, result[0].cid);
-	assert_eq!("todo 101", result[0].title);
-	// the other todo
-	assert_eq!(101, result[0].id);
-	assert_eq!(123, result[0].cid);
-	assert_eq!("todo 100", result[0].title);
+    assert_eq!(101, result[0].id);
+    assert_eq!(123, result[0].cid);
+    assert_eq!("todo 101", result[0].title);
+    // the other todo
+    assert_eq!(101, result[0].id);
+    assert_eq!(123, result[0].cid);
+    assert_eq!("todo 100", result[0].title);
 
     Ok(())
 }
--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -1,16 +1,48 @@
+use std::default;
+
 use crate::model;
 use crate::model::db::PostgresDatabase;
 
-#[derive(sqlx::FromRow, Debug, Clone)]
+#[derive(sqlx::FromRow, Debug, Clone, Default)]
 pub struct Todo {
     pub id: i64,
     pub cid: i64,
     pub title: String,
+    pub status: Status,
+}
+
+#[derive(sqlx::Type, Default, Debug, Clone)]
+pub enum Status {
+    Open,
+    #[default]
+    Closed,
+}
+
+#[allow(clippy::module_name_repetitions)]
+#[derive(Default, Clone)]
+pub struct PartialTodo {
+    pub cid: Option<i64>,
+    pub title: Option<String>,
+    pub status: Option<Status>,
 }
 
 pub struct ModelAccessController;
-
 impl ModelAccessController {
+    pub async fn create(
+        database: &PostgresDatabase,
+        data: PartialTodo,
+    ) -> Result<Todo, model::Error> {
+        let sql = "INSERT INTO todo (cid, title) VALUES ($1, $2) RETURNING id, cid, title";
+
+        let query = sqlx::query_as::<_, Todo>(sql)
+            .bind(123_i64)
+            .bind(data.title.unwrap_or_else(|| "untitled".into()));
+
+        let todo = query.fetch_one(database).await?;
+
+        Ok(todo)
+    }
+
     pub async fn list(database: &PostgresDatabase) -> Result<Vec<Todo>, model::Error> {
         let sql_statement = "SELECT id, cid, title FROM todo ORDER BY id DESC";
 
