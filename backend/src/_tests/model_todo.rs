0f83d523132a7510d623e51675d2684e7e646b9d
Neal-C <pop6d531@duck.com>
2023-09-25 21:45:07 +0200

    add(update-test): started adding tests

--- backend/src/_tests/model_todo.rs
+++ backend/src/_tests/model_todo.rs
@@ -69,3 +69,43 @@ async fn model_todo_get() -> Result<(), Box<dyn std::error::Error>> {
     assert_eq!(todo.status, todo::Status::Closed);
     Ok(())
 }
+
+#[tokio::test]
+async fn model_todo_update_ok() -> Result<(), Box<dyn std::error::Error>> {
+    // ARRANGE
+    let database = initialize_database().await?;
+
+    let data_fixture = PartialTodo {
+        title: Some(String::from("test - model_todo_create 1")),
+        ..PartialTodo::default()
+    };
+
+    let user_context = user_context_from_token("123").await?;
+
+    let todo_created =
+        ModelAccessController::create(&database, &user_context, data_fixture.clone()).await?;
+
+    assert!(todo_created.id >= 1000, "ID should be >= 1000");
+    assert_eq!(data_fixture.title.unwrap(), todo_created.title);
+    assert_eq!(todo_created.status, todo::Status::Open);
+
+    let update_data_fixture = PartialTodo {
+        title: Some(String::from("test - model_todo_update_ok")),
+        ..PartialTodo::default()
+    };
+    // ACT
+
+    let todo_updated = ModelAccessController::update(
+        &database,
+        &user_context,
+        todo_created.id,
+        update_data_fixture.clone(),
+    )
+    .await?;
+
+    let list_result = ModelAccessController::list(&database, &user_context).await?;
+
+    assert_eq!(list_result.len(), 3);
+
+    Ok(())
+}
